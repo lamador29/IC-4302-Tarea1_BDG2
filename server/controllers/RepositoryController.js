9b0b889cb4c72cb1c8551890ff793c79f28d0421
Wesley <129779497+WesleyEsq@users.noreply.github.com>
2024-09-20 12:40:04 -0600

    Update RepositoryController.js

--- server/controllers/RepositoryController.js
+++ server/controllers/RepositoryController.js
@@ -1,36 +1,72 @@
 const mongoose = require('mongoose');
 
-//Acceder a models bien tedioso
+//Acceder a models
 const path = require('path');
-const re = '/path/to/current/directory';
+const re = 'server//controllers';
 const parentDir = path.dirname(re);
 const modelsDir = path.join(parentDir, 'models');
 
+
 //Modelos
 const repository = path.join(modelsDir, 'repository')
+
 
 //Conexion a MongoDB ATLAS
 const uri = "mongodb+srv://lector:1234@tarea1.acsgw.mongodb.net/RPDB?retryWrites=true&w=majority&appName=Tarea1";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
   .catch(err => console.log(err));
-  
+
+
 //Crear repositorio: lo manda a la BD y retorna el repositorio
-async function createRepository(title, isPublic, user) {
+async function createRepository(title, isPublic, userString, tagsString) {
     try {
-        const newRepository = new repository({
+        const users = [userString];
+        const tags = tagsString ? tagsString.split(',').map(tag => tag.trim()) : [];
+        const newRepository = new Repository({
             title: title,
             isPublic: isPublic,
-            users: [user]
+            users: users,
+            tags: tags      
         });
-        // Save the new document to the database
         const savedRepository = await newRepository.save();
-        console.log('New repository created:', savedRepository);
+        console.log('Nuevo repositorio:', savedRepository);
         return savedRepository;
     } catch (error) {
-        console.error('Error creating repository:', error);
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+//Regrese todos los repositorios de un usuario
+async function RepositoriesOfAnUser(user) {
+    try {
+        // Find repositories where the user is in the users array
+        const repositories = await Repository.find({ users: user });
+
+        console.log(`Repositorios del usuario ${user}:`, repositories);
+        return repositories;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+//Para busquedas
+async function search(titleStart) {
+    try {
+        const repositories = await Repository.find({
+            title: { $regex: `^${titleStart}`, $options: 'i' }, 
+            isPublic: true  
+        });
+        console.log(`Resultados"${titleStart}":`, repositories);
+        return repositories;
+    } catch (error) {
+        console.error('Error:', error);
         throw error;
     }
 }
 
 export{ createRepository};
+export{ RepositoriesOfAnUser};
+export{ search}
