cc7be1489aa7de55bbd7ec50ef6cd93d5d9fd91b
ipicado17 <isaac.picado1@gmail.com>
2024-09-22 12:24:44 -0600

    Actualización de registerController
    manejo de errores cuando el usuario ya existe en la base de datos

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -6,20 +6,36 @@ const neo4jClient = require('../db/neo4j');
 const saltRounds = 10;
 
 exports.registerUser = async (req, res) => {
-  console.log('Datos recibidos:', req.body); 
+  console.log('Datos recibidos:', req.body);
   const { email, password, username } = req.body;
 
   try {
-    const hash = await bcrypt.hash(password, saltRounds);
-
-    const encryptedEmail = encrypt(email);
     const hashedUsername = hashUsername(username);
+    const existingUserKey = new Aerospike.Key('test', 'users', hashedUsername);
 
-    const key = new Aerospike.Key('test', 'users', hashedUsername);
-    const record = { username: hashedUsername, password: hash, email: encryptedEmail };
+    try {
+      await client.get(existingUserKey);
+      return res.status(409).send('El nombre de usuario ya existe'); // Conflict
+    } catch (getError) {
+      if (getError.code !== Aerospike.status.ERR_RECORD_NOT_FOUND) {
+        // Handle other potential errors
+        console.error('Error al verificar existencia del usuario:', getError);
+        return res.status(500).send('Error al verificar el usuario');
+      }
+      // Proceed to register the user
+    }
 
-    await client.put(key,record);
-    console.log('Registro guardado en Aerospike:', record);
+    const hash = await bcrypt.hash(password, saltRounds);
+    const encryptedEmail = encrypt(email);
+
+    const newUserRecord = {
+      username: hashedUsername,
+      password: hash,
+      email: encryptedEmail
+    };
+
+    await client.put(existingUserKey, newUserRecord);
+    console.log('Registro guardado en Aerospike:', newUserRecord);
 
     const session = neo4jClient.session();
     const result = await session.run(
@@ -29,6 +45,7 @@ exports.registerUser = async (req, res) => {
     const userNode = result.records[0].get('u');
     console.log('Nodo usuario creado en Neo4j:', userNode);
     await session.close();
+
     res.status(201).send('Registro exitoso');
   } catch (error) {
     console.error('Error durante el registro:', error);
